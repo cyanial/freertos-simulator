cb83942a525085d80caa82ff5592b425cf73b652
cyanial <chenjie3323@sjtu.edu.cn>
2021-12-12 19:39:38 +0800

    add files

--- main.c
+++ main.c
@@ -1,13 +1,70 @@
+/* Standard includes. */
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/select.h>
+#include <pthread.h>
 
+/* FreeRTOS Kernel include. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "semphr.h"
+
+#include "console.h"
+
+/* Function Prototypes. */
+static void handle_sigint(int signal);
+
+/* Priorities at which the tasks are created. */
+#define mainQUEUE_RECEIVE_TASK_PRIORITY    ( tskIDLE_PRIORITY + 2 )
+#define mainQUEUE_SEND_TASK_PRIORITY       ( tskIDLE_PRIORITY + 1 )
+
+/* The rate at which data is sent to the queue.  The times are converted from
+ * milliseconds to ticks using the pdMS_TO_TICKS() macro. */
+#define mainTASK_SEND_FREQUENCY_MS         pdMS_TO_TICKS( 200UL )
+#define mainTIMER_SEND_FREQUENCY_MS        pdMS_TO_TICKS( 2000UL )
+
+/* The number of items the queue can hold at once. */
+#define mainQUEUE_LENGTH                   ( 2 )
+
+/* The values sent to the queue receive task from the queue send task and the
+ * queue send software timer respectively. */
+#define mainVALUE_SENT_FROM_TASK           ( 100UL )
+#define mainVALUE_SENT_FROM_TIMER          ( 200UL )
+
+/* Task prototypes .*/
+static void prvQueueReceiveTask(void *pvParameters);
+static void prvQueueSendTask(void *pvParameters);
+static void 
 
 
 int main()
 {
+    // SIGINT is not blocked bu the posix port
+    // signal(SIGINT, handle_sigint);
 
-    vTaskStartScheduler();
-
-
+    sonsole_init();
+    console_print("Application start ...\n");
 
     for ( ;; );
+    
     return 0;
+}
+
+static void handle_sigint(int signal)
+{
+    // int xReturn;
+
+    // // xReturn = chdir(BUILD);
+
+    // if (xReturn == -1)
+    // {
+        
+    // }
+
+    // exit(2);
 }
